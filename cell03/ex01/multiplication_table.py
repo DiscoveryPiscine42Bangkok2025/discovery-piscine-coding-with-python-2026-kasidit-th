2aa5d6301e6ecc241d1bff11d92f8eaa2b98f3a7
kasidit-th <kasidit.thong@gmail.com>
2026-02-09 23:19:04 +0700

    4

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number\n"))
+
+i = 0
+while i < 10:
+    print(i, "x", number, "=", i * number)
+    i += 1
---
+++ cell04/ex03/float.py
@@ -0,0 +1,9 @@
+number = input("Give me a number: ")
+
+if "." in number:
+    if float(number).is_integer():
+        print("This number is an integer.")
+    else:
+        print("This number is a decimal.")
+else:
+    print("This number is an integer.")
